b181213210297984bc942c2482f8b1af87fbc614
Jerry <yzi@outlook.com>
2022-11-29 12:40:31 +0900

    Update get_sources.sh

--- get_sources.sh
+++ get_sources.sh
@@ -7,7 +7,6 @@ if [ -d "$SOURCES_PATH" ]; then
 fi
 
 echo "$SOURCES_PATH"
-
 mkdir -p $SOURCES_PATH
 
 cd $SOURCES_PATH
@@ -29,7 +28,7 @@ git clone --depth 1 https://aomedia.googlesource.com/aom
 echo -e "\e[1;44m GET AMF  \e[0m"
 git clone --depth 1 https://github.com/GPUOpen-LibrariesAndSDKs/AMF.git 
 
-echo -e "\e[1;44m brolti AMF  \e[0m"
+echo -e "\e[1;44m brolti \e[0m"
 git clone --depth 1 https://github.com/google/brotli.git
 
 echo -e "\e[1;44m GET freetype  \e[0m"
@@ -49,3 +48,25 @@ git clone --depth 1 https://chromium.googlesource.com/webm/libvpx
 
 echo -e "\e[1;44m GET fdk-aac  \e[0m"
 git clone --depth 1 https://github.com/mstorsjo/fdk-aac.git
+
+echo -e "\e[1;44m GET xavs  \e[0m"
+git clone --depth 1 https://github.com/Distrotech/xavs.git
+
+echo -e "\e[1;44m GET frei0r  \e[0m"
+git clone --depth 1 https://github.com/dyne/frei0r.git
+
+echo -e "\e[1;44m GET libcaca  \e[0m"
+git clone --depth 1 https://github.com/cacalabs/libcaca.git
+
+echo -e "\e[1;44m GET kvazaar  \e[0m"
+git clone --depth 1 https://github.com/ultravideo/kvazaar.git
+
+echo -e "\e[1;44m GET xavs2  \e[0m"
+git clone --depth 1 https://github.com/pkuvcl/xavs2.git
+
+echo -e "\e[1;44m GET davs2  \e[0m"
+git clone --depth 1 https://github.com/pkuvcl/davs2.git
+
+echo -e "\e[1;44m GET mp3lame  \e[0m"
+wget https://sourceforge.net/projects/lame/files/lame/3.100/lame-3.100.tar.gz/download -O lame-3.100.tar.gz
+tar zxvf lame-3.100.tar.gz & mv lame-3.100 mp3lame
