288e5f5cf9294a66ce204838f8993b3d3672a0e8
yzi2004 <yzi@outlook.com>
2022-12-25 16:30:25 +0900

    for test

---
+++ macos/do_test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ARCH=arm64
+TARGET_SDK=iphoneos
+SDK_PATH=`xcrun --show-sdk-path --sdk iphoneos`
+BASEDIR=/Volumes/dev/ffmpeg_compile/macos
+
+. ${BASEDIR}/script/ios-common.sh
+
+LIB_NAME="x264"
+
+set_toolchain_clang_paths ${LIB_NAME}
+
---
+++ macos/script/ios_common.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+export DOWNLOAD_DIR="${BASEDIR}/download"
+
+download() {
+  if [ ! -d "${DOWNLOAD_DIR}" ]; then
+    mkdir -p "${DOWNLOAD_DIR}"
+  fi
+
+  (curl --fail --location $1 -o ${DOWNLOAD_DIR}/$2 1>>${BASEDIR}/build.log 2>&1)
+
+  local RC=$?
+
+  if [ ${RC} -eq 0 ]; then
+    echo -e "\nDEBUG: Downloaded $1 to ${DOWNLOAD_DIR}/$2\n" 1>>${BASEDIR}/build.log 2>&1
+  else
+    rm -f ${DOWNLOAD_DIR}/$2 1>>${BASEDIR}/build.log 2>&1
+
+    echo -e -n "\nINFO: Failed to download $1 to ${DOWNLOAD_DIR}/$2, rc=${RC}. " 1>>${BASEDIR}/build.log 2>&1
+
+    if [ "$3" == "exit" ]; then
+      echo -e "DEBUG: Build will now exit.\n" 1>>${BASEDIR}/build.log 2>&1
+      exit 1
+    else
+      echo -e "DEBUG: Build will continue.\n" 1>>${BASEDIR}/build.log 2>&1
+    fi
+  fi
+
+  echo ${RC}
+}
+
+get_arch_name() {
+    case $1 in
+        0) echo "arm64" ;;
+        1) echo "arm64e" ;;
+        2) echo "x86-64" ;;
+    esac
+}
+
+get_target_host() {
+      case ${ARCH} in
+        x86-64-mac-catalyst)
+            echo "x86_64-apple-ios13.0-macabi"
+        ;;
+        *)
+            echo "$(get_target_arch)-ios-darwin"
+        ;;
+    esac
+}
+
+get_build_host() {
+    echo "$(get_target_arch)-ios-darwin"
+}
+
+get_target_build_directory() {
+    case ${ARCH} in
+        x86-64)
+            echo "ios-x86_64"
+        ;;
+        x86-64-mac-catalyst)
+            echo "ios-x86_64-mac-catalyst"
+        ;;
+        *)
+            echo "ios-${ARCH}"
+        ;;
+    esac
+}
+
+get_target_arch() {
+    case ${ARCH} in
+        arm64 | arm64e)
+            echo "aarch64"
+        ;;
+        x86-64 | x86-64-mac-catalyst)
+            echo "x86_64"
+        ;;
+        *)
+            echo "${ARCH}"
+        ;;
+    esac
+}
+
+set_toolchain_clang_paths() {
+
+    if [ ! -f "${DOWNLOAD_DIR}/gas-preprocessor.pl" ]; then
+
+        DOWNLOAD_RESULT=$(download "https://github.com/libav/gas-preprocessor/raw/master/gas-preprocessor.pl" "gas-preprocessor.pl" "exit")
+        if [[ ${DOWNLOAD_RESULT} -ne 0 ]]; then
+            echo 'tt'
+            exit 1
+        fi
+        (chmod +x ${DOWNLOAD_DIR}/gas-preprocessor.pl 1>>${BASEDIR}/build.log 2>&1) || exit 1
+
+        # patch gas-preprocessor.pl against the following warning
+        # Unescaped left brace in regex is deprecated here (and will be fatal in Perl 5.32), passed through in regex; marked by <-- HERE in m/(?:ld|st)\d\s+({ <-- HERE \s*v(\d+)\.(\d[bhsdBHSD])\s*-\s*v(\d+)\.(\d[bhsdBHSD])\s*})/ at /Users/taner/Projects/mobile-ffmpeg/.tmp/gas-preprocessor.pl line 1065.
+        sed -i .tmp "s/s\+({/s\+(\\\\{/g;s/s\*})/s\*\\\\})/g" ${DOWNLOAD_DIR}/gas-preprocessor.pl
+    fi
+}
+
