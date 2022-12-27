cfeafa8a2ce9f5bf0ee706053b603d32fa89fcf5
Jerry <yzi@outlook.com>
2022-12-27 11:04:02 +0900

    Update do_test.sh

--- macos/do_test.sh
+++ macos/do_test.sh
@@ -5,7 +5,7 @@ TARGET_SDK=iphoneos
 SDK_PATH=`xcrun --show-sdk-path --sdk iphoneos`
 BASEDIR=/Volumes/dev/ffmpeg_compile/macos
 
-. ${BASEDIR}/script/ios_common.sh
+. ./script/ios_common.sh
 
 LIB_NAME="x264"
 
