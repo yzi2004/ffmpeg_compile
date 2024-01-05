510094faa0e4c854c57a0e0d4d319edd84caa7b9
Jerry <yzi@outlook.com>
2024-01-05 15:13:02 +0900

    Update get_source.sh

--- cross_linux/get_source.sh
+++ cross_linux/get_source.sh
@@ -3,17 +3,21 @@ set -x
 
 sources_path="$(pwd)/sources"
 
+if [ -d "${sources_path}" ]; then
+    rm -rf "${sources_path}"
+fi
+
 mkdir -p $sources_path
 
 cd $sources_path
 
-x264_git="https://code.videolan.org/videolan/x264.git"
-x264_ver="stable"
-git clone --depth 1 $x64_git -b $x264_ver x264
+X264_git="https://code.videolan.org/videolan/x264.git"
+X264_ver="stable"
+git clone --depth 1 $X264_git -b $X264_ver x264
 
-x265_git="https://bitbucket.org/multicoreware/x265_git.git"
-x265_ver="3.5"
-git clone $265_git -b $x65_ver x265
+X265_git="https://bitbucket.org/multicoreware/x265_git.git"
+X265_ver="3.5"
+git clone $X265_git -b $X265_ver x265
 
 ffmpeg_git="https://git.ffmpeg.org/ffmpeg.git"
 ffmpeg_release="n6.1"
