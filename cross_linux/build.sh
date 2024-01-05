054909beefc5d193a56cf7764ef504a7216ea739
Jerry <yzi@outlook.com>
2024-01-05 16:24:12 +0900

    Update build.sh

--- cross_linux/build.sh
+++ cross_linux/build.sh
@@ -41,50 +41,50 @@ pushd x265
 mkdir -p 8bit 10bit 12bit 
 cd 12bit
 
-cmake -G "Ninja" 
-		-DCMAKE_TOOLCHAIN_FILE="$config_dir/cross_for_windows.cmake" \
-		-DCMAKE_INSTALL_PREFIX=$dist_path \
-		-DENABLE_SHARED=OFF \
-		-DENABLE_CLI=OFF \
-		-DEXPORT_C_API=ON \
-		-DSTATIC_LINK_CRT=ON \
-		-DHIGH_BIT_DEPTH=ON \
-		-DCMAKE_POLICY_DEFAULT_CMP0091=NEW \
-		-DENABLE_ASSEMBLY=ON \
-		-DMAIN12=ON \
-    $SOURCES_PATH/x265/source 
+cmake -G "Ninja" \
+	-DCMAKE_TOOLCHAIN_FILE="$config_dir/cross_for_windows.cmake" \
+	-DCMAKE_INSTALL_PREFIX=$dist_path \
+	-DENABLE_SHARED=OFF \
+	-DENABLE_CLI=OFF \
+	-DEXPORT_C_API=ON \
+	-DSTATIC_LINK_CRT=ON \
+	-DHIGH_BIT_DEPTH=ON \
+	-DCMAKE_POLICY_DEFAULT_CMP0091=NEW \
+	-DENABLE_ASSEMBLY=ON \
+	-DMAIN12=ON \
+	$SOURCES_PATH/x265/source 
 ninja 
 
 cd ../10bit
 
-cmake -G "Ninja" 
-		-DCMAKE_TOOLCHAIN_FILE="$config_dir/cross_for_windows.cmake" \
-		-DCMAKE_INSTALL_PREFIX=$dist_path \
-		-DENABLE_SHARED=OFF \
-		-DENABLE_CLI=OFF \
-		-DEXPORT_C_API=ON \
-		-DSTATIC_LINK_CRT=ON \
-		-DHIGH_BIT_DEPTH=ON \
-		-DCMAKE_POLICY_DEFAULT_CMP0091=NEW \
-		-DENABLE_ASSEMBLY=ON \
-		-DMAIN12=OFF \
-    $SOURCES_PATH/x265/source 
+cmake -G "Ninja" \
+	-DCMAKE_TOOLCHAIN_FILE="$config_dir/cross_for_windows.cmake" \
+	-DCMAKE_INSTALL_PREFIX=$dist_path \
+	-DENABLE_SHARED=OFF \
+	-DENABLE_CLI=OFF \
+	-DEXPORT_C_API=ON \
+	-DSTATIC_LINK_CRT=ON \
+	-DHIGH_BIT_DEPTH=ON \
+	-DCMAKE_POLICY_DEFAULT_CMP0091=NEW \
+	-DENABLE_ASSEMBLY=ON \
+	-DMAIN12=OFF \
+	$SOURCES_PATH/x265/source 
 ninja 
 
 cd ../8bit
 ln -sf ../10bit/libx265.a libx265_main10.a
 ln -sf ../12bit/libx265.a libx265_main12.a
-cmake -G "Ninja" 
-		-DCMAKE_TOOLCHAIN_FILE="$config_dir/cross_for_windows.cmake" \
-		-DCMAKE_INSTALL_PREFIX=$dist_path \
-		-DENABLE_SHARED=OFF \
-		-DENABLE_CLI=OFF \
-		-DEXPORT_C_API=ON \
-		-DSTATIC_LINK_CRT=ON \
-    -DEXTRA_LIB="x265_main10.a;x265_main12.a" \
-    -DEXTRA_LINK_FLAGS=-L. \
-    -DLINKED_10BIT=ON  \
-    -DLINKED_12BIT=ON \
-    $SOURCES_PATH/x265/source 
+cmake -G "Ninja" \
+	-DCMAKE_TOOLCHAIN_FILE="$config_dir/cross_for_windows.cmake" \
+	-DCMAKE_INSTALL_PREFIX=$dist_path \
+	-DENABLE_SHARED=OFF \
+	-DENABLE_CLI=OFF \
+	-DEXPORT_C_API=ON \
+	-DSTATIC_LINK_CRT=ON \
+	-DEXTRA_LIB="x265_main10.a;x265_main12.a" \
+	-DEXTRA_LINK_FLAGS=-L. \
+	-DLINKED_10BIT=ON  \
+	-DLINKED_12BIT=ON \
+	$SOURCES_PATH/x265/source 
 ninja
 
