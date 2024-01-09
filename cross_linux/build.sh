7f048e0db3286c661b03daddd932d5f1cb3d8dec
Jerry <yzi@outlook.com>
2024-01-09 21:33:56 +0900

    Update build.sh

--- cross_linux/build.sh
+++ cross_linux/build.sh
@@ -105,16 +105,7 @@ popd
 #popd
 
 #■■■■■■■compile fdk-aac
-mkdir -p fdk-aac
-pushd fdk-aac
-
-cmake -G "Ninja" \
-	-DCMAKE_TOOLCHAIN_FILE="$config_dir/cross_for_windows.cmake" \
-	-DCMAKE_INSTALL_PREFIX=$dist_path \
-	-DBUILD_SHARED_LIBS=OFF \
-	$sources_path/fdk-aac
-ninja && cmake --install . 
-popd
+cmake_compile "fdk-aac" "-DBUILD_SHARED_LIBS=OFF"
 
 #■■■■■■■compile libaom
 mkdir -p libaom
@@ -247,3 +238,21 @@ popd
 
 #make -j $threads
 #make install
+
+function cmake_compile(){
+	if [ -d "${build_path}/$1" ]; then
+	    rm -rf "$build_path/$1"
+	fi
+	
+	mkdir -p $1
+	pushd $1
+	
+	cmake -G "Ninja" \
+	        -DCMAKE_TOOLCHAIN_FILE="$config_dir/cross_for_windows.cmake" \
+	        -DCMAKE_INSTALL_PREFIX=$dist_path \
+	        $2 \
+	        $sources_path/$1
+	
+	ninja && cmake --install .
+	popd
+}
