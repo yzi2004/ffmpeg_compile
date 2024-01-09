ec00895c4803e749099b0630ba0d19f75731182d
Jerry <yzi@outlook.com>
2024-01-09 21:43:49 +0900

    Create compiler.sh

---
+++ cross_linux/compiler.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cmake_compile(){
+	if [ -d "${build_path}/$1" ]; then
+	    rm -rf "$build_path/$1"
+	fi
+	
+	mkdir -p $1
+	pushd $1
+	
+	cmake -G "Ninja" \
+	        -DCMAKE_TOOLCHAIN_FILE="$config_dir/cross_for_windows.cmake" \
+	        -DCMAKE_INSTALL_PREFIX=$dist_path \
+	        $2 \
+	        $sources_path/$1
+	
+	ninja && cmake --install .
+	popd
+}
