45348bb0e629a056e9aafbc79576748fe1ee07fb
Jerry <yzi@outlook.com>
2024-01-09 21:45:59 +0900

    Update build.sh

--- cross_linux/build.sh
+++ cross_linux/build.sh
@@ -15,6 +15,8 @@ threads="8"
 
 export PKG_CONFIG_PATH="$dist_path/lib/pkgconfig"
 
+. compiler.sh
+
 if [ -d "${build_path}" ]; then
     rm -rf "${build_path}"
 fi
@@ -104,24 +106,6 @@ popd
 
 #■■■■■■■compile fdk-aac
 cmake_compile "fdk-aac" "-DBUILD_SHARED_LIBS=OFF"
-
-cmake_compile(){
-	if [ -d "${build_path}/$1" ]; then
-	    rm -rf "$build_path/$1"
-	fi
-	
-	mkdir -p $1
-	pushd $1
-	
-	cmake -G "Ninja" \
-	        -DCMAKE_TOOLCHAIN_FILE="$config_dir/cross_for_windows.cmake" \
-	        -DCMAKE_INSTALL_PREFIX=$dist_path \
-	        $2 \
-	        $sources_path/$1
-	
-	ninja && cmake --install .
-	popd
-}
 
 
 
