fc59f82bafd6e9e7b0652bedaf7dc34ba682d660
Jerry <yzi@outlook.com>
2024-01-12 11:14:59 +0900

    Update init_env.sh

--- cross_linux/init_env.sh
+++ cross_linux/init_env.sh
@@ -2,4 +2,8 @@
 
 sudo apt install cmake ninja-build nasm meson
 
+git config --global user.name "yzi2004"
+git config --global user.email "yzi@outlook.com"
+
 git config --global advice.detachedHead false
+git config --global credential.helper store
