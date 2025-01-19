7f1458af4639121ed0ecaaf2847848a45bc6b3b7
Jerry <yzi@outlook.com>
2025-01-19 23:39:53 +0900

    Update init_env.sh

--- cross_linux/init_env.sh
+++ cross_linux/init_env.sh
@@ -2,6 +2,10 @@
 
 sudo apt install cmake ninja-build nasm meson
 
+if [  -d "/opt/llvm-mingw-ucrt-x86_64" ]; then
+        sudo rm -rf /opt/llvm-mingw-ucrt-x86_64
+fi
+
 wget https://github.com/mstorsjo/llvm-mingw/releases/download/20250114/llvm-mingw-20250114-ucrt-ubuntu-20.04-x86_64.tar.xz
 tar xf llvm-mingw-20250114-ucrt-ubuntu-20.04-x86_64.tar.xz
 rm llvm-mingw-20250114-ucrt-ubuntu-20.04-x86_64.tar.xz
